b71afecc00afaf7b22aeb1e2f5993b626e3fe6e0
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-07 12:37:48 +0300

    Add DFS function for grid traversal based on height constraints

--- pacificAtlanticWaterFlowSolveDFS.py
+++ pacificAtlanticWaterFlowSolveDFS.py
@@ -4,3 +4,13 @@ class Solution:
     def pacificAtlantic(self,heights:List[List[int]])->List[List[int]]:
         ROWS,COLUMNS=len(heights),len(heights[0])
         pacific,atlantic=set(),set()
+        
+        def DFS(r,c,visit,prevHeight):
+            if(r,c)in visit or r<0 or c<0 or r==ROWS or c==COLUMNS or heights[r][c]<prevHeight:
+                return
+            visit.add((r,c))
+            DFS(r+1,c,visit,heights[r][c])
+            DFS(r-1,c,visit,heights[r][c])
+            DFS(r,c+1,visit,heights[r][c])
+            DFS(r,c-1,visit,heights[r][c]) 
+
