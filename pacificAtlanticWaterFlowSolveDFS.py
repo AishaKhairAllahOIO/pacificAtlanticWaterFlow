0d0be4521b95af880721d4e24fac759d20d62c27
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-07 13:35:42 +0300

    Testing code

--- pacificAtlanticWaterFlowSolveDFS.py
+++ pacificAtlanticWaterFlowSolveDFS.py
@@ -30,5 +30,17 @@ class Solution:
                     coordinates.append([r,c])
         return coordinates
     
+if __name__=="__main__":
+    heights=[
+        [1, 2, 2, 3, 5],
+        [3, 2, 3, 4, 4],
+        [2, 4, 5, 3, 1],
+        [6, 7, 1, 4, 5],
+        [5, 1, 1, 2, 4]
+    ]
+    sol=Solution()
+    answer=sol.pacificAtlantic(heights)
+    print("coordinates: ",answer)
+    
 
 
